121a825eb12fb05a7ddf221058cb4c4f641d9553
misspelz <pelumiiadetoye@gmail.com>
2024-09-30 20:36:17 +0100

    delete modal

---
+++ src/utils/ImageData.ts
@@ -0,0 +1,43 @@
+export  const imageData = [
+  {
+    src: "https://images.pexels.com/photos/27680935/pexels-photo-27680935/free-photo-of-a-woman-standing-outside-of-a-building-with-a-yellow-door.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "You are standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/26836559/pexels-photo-26836559/free-photo-of-foamy-wave-crashing-onto-shore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Old man standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/27680935/pexels-photo-27680935/free-photo-of-a-woman-standing-outside-of-a-building-with-a-yellow-door.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Hen standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/28297570/pexels-photo-28297570/free-photo-of-a-wooden-door-with-vines-growing-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Cat standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/27680935/pexels-photo-27680935/free-photo-of-a-woman-standing-outside-of-a-building-with-a-yellow-door.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Boy standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/17829466/pexels-photo-17829466/free-photo-of-view-of-birds-flying-over-a-beach-with-sea-stacks.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Girl standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/27680935/pexels-photo-27680935/free-photo-of-a-woman-standing-outside-of-a-building-with-a-yellow-door.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Man standing outside of a building",
+  },
+  {
+    src: "https://images.pexels.com/photos/8887894/pexels-photo-8887894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "woman-standing-outside-of-a-building",
+    description: "Woman standing outside of a building",
+  },
+  // Add more items as necessary
+];
