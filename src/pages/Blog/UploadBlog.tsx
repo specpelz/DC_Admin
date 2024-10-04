6a561558b13e1d44ed59a42d1d3f29a1f0ee9174
Chimaux <ugoppromise@gmail.com>
2024-10-04 20:58:07 +0100

    Implement text editor component

--- src/pages/Blog/UploadBlog.tsx
+++ src/pages/Blog/UploadBlog.tsx
@@ -1,39 +1,17 @@
+import RichEditor from "@components/dashboard/richEditor/RichEditor";
 import useBlogStore from "@store/blog";
 import { Button } from "antd";
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { MdOutlineCloudUpload } from "react-icons/md";
-import { createEditor } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
-import { BaseEditor,
-  //  Descendant 
-  } from 'slate'
-import { ReactEditor } from 'slate-react'
-
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
-type CustomText = { text: string }
-
-declare module 'slate' {
-  interface CustomTypes {
-    Editor: BaseEditor & ReactEditor
-    Element: CustomElement
-    Text: CustomText
-  }
-}
 
 
-const initialValue: CustomElement[] = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-];
 
 
 const UploadBlog = () => {
 
-  const [editor] = useState(() => withReact(createEditor()))
+
 
     const set_component = useBlogStore(
         (state) => state.set_component
@@ -150,10 +128,9 @@ const UploadBlog = () => {
 
 
 
-<div>
-<Slate editor={editor} initialValue={initialValue} >
-<Editable />
-</Slate>
+<div className="w-full mt-[30px]">
+{/* Rich text editor here */}
+<RichEditor/>
 </div>
 
 
