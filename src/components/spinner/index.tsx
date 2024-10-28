93374a0de630afbe2cdbef8f410044920143829e
chimaux <ugoppromise@gmail.com>
2024-10-28 09:34:35 -0400

    ipdated Spinner component

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -2,7 +2,7 @@ import { Spin } from 'antd'
 
 const Spinner = () => {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
+    <div className='h-screen w-[100%] flex justify-center items-center'>
 
         <Spin size="large" />
     </div>
